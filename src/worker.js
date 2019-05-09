f676e78bea0af5ad627a8bb686757f4a4ef645f7
thebigbrain <uibrick@126.com>
2019-05-09 10:04:44 +0800

    .

---
+++ src/worker.js
@@ -0,0 +1,21 @@
+require('v8-compile-cache');
+const Pipeline = require('./Pipeline');
+
+let pipeline;
+
+function init(options) {
+  pipeline = new Pipeline(options || {});
+  Object.assign(process.env, options.env || {});
+}
+
+async function run(path, isWarmUp) {
+  try {
+    return await pipeline.process(path, isWarmUp);
+  } catch (e) {
+    e.fileName = path;
+    throw e;
+  }
+}
+
+exports.init = init;
+exports.run = run;
