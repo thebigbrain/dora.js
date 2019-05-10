b0a0ba9a2474f71f76f7cce60c2e220a73ee30d2
thebigbrain <uibrick@126.com>
2019-05-10 09:16:38 +0800

    .

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 // Node 8 supports native async functions - no need to use compiled code!
-module.exports = parseInt(process.versions.node, 10) < 8
-  ? require('./lib/Bundler')
-  : require('./src/Bundler');
+module.exports = require('./src/Bundler');
